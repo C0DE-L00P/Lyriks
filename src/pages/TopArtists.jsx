4ebf05fbb5227ff13f1ee82f4ec97ee173db015f
Mohamed Gamal <jaiden90@gmail.com>
2022-10-16 15:44:02 +0200

    Implement Top Artists logic

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-import { ArtistCard, Error, Loader } from '../components';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { ArtistCard, Error, Loader } from "../components";
+import {
+  useGetArtistDetailsQuery,
+  useGetTopChartsQuery,
+} from "../redux/services/shazamCore";
 
 const TopArtists = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
@@ -11,10 +15,14 @@ const TopArtists = () => {
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Top artists</h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Top artists
+      </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {data?.map((track) => <ArtistCard key={track.key} track={track} />)}
+        {data?.map((track) => (
+          <ArtistCard key={track.key} track={track} />
+        ))}
       </div>
     </div>
   );
